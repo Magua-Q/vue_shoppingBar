aa36cc6baf7ef79715d37c59d58ea5280d79c2e5
xukang <757946840@qq.com>
2018-09-03 21:24:30 +0800

    初始化项目页面

---
+++ index.js
@@ -0,0 +1,28 @@
+// 初始化实例
+var app = new Vue({
+    el:'#app',
+    data:{
+        list:[
+            {
+                id:1,
+                name:'iphone 7',
+                price:6188,
+                count:1
+            },
+            {
+                id:2,
+                name:'ipad Pro',
+                price:5188,
+                count:1
+            },
+            {
+                id:3,
+                name:'MacBook Pro',
+                price:21488,
+                count:1
+            }
+        ]
+    },
+    computed:{},
+    methods:{}
+})
